73175a8375c4c104c6656c31ab522754687479d8
Armando Fernandez <codefilez@gmail.com>
2025-03-07 17:16:13 -0800

    Models created : Bill, BillType, Payment

---
+++ Models/BillType.cs
@@ -0,0 +1,17 @@
+﻿namespace BillFlow.Models
+{
+    public class BillType
+    {
+        public BillType(){}
+
+        public BillType(string name, string? description = null)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
