852b066b8a443fb9b8fc1793c514a4891085dcc1
Armando Fernandez <codefilez@gmail.com>
2025-03-14 16:35:12 -0700

    Updated Body MainLayout to include RadenToolTip Component (Required to render the ToolTips when they are triggered.

--- Services/BillFlowToolTipService.cs
+++ Services/BillFlowToolTipService.cs
@@ -6,13 +6,17 @@ namespace BillFlow.Services
     public class BillFlowToolTipService
     {
         private readonly Radzen.TooltipService _toolTipService;
-        public BillFlowToolTipService(Radzen.TooltipService toolTipService) 
+        public BillFlowToolTipService(Radzen.TooltipService toolTipService)
         {
             _toolTipService = toolTipService;
         }
 
-        public void ShowTooltip(ElementReference elementReference, string toolTipText, TooltipOptions options = null)
+        public async Task ShowTooltip(ElementReference elementReference, string toolTipText, TooltipOptions options = null)
         {
+            await Task.Yield();
+
+            options = options ?? new TooltipOptions() {Position = TooltipPosition.Top };
+
             _toolTipService.Open(elementReference, toolTipText, options);
         }
     }
